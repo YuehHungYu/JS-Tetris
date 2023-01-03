2b85b1d857fa3c30577c63d7b4320ca365f08ca6
hung yu <yuehyu1028@gmail.com>
2023-01-03 16:00:06 +0800

    create Piece

--- app.js
+++ app.js
@@ -32,5 +32,55 @@ function collide(arena,player){
 }
 
 function createMatrix(w,h){
-  
+  const matrix=[];
+  while(h--){
+    matrix.push(new Array(w).fill(0));
+  }
+  return matrix;
+}
+
+function createPiece(type){
+  if(type==="I"){
+    return[
+      [0, 1, 0, 0],
+      [0, 1, 0, 0],
+      [0, 1, 0, 0],
+      [0, 1, 0, 0],
+    ];
+  }else if(type==="L"){
+    return[
+      [0, 2, 0],
+      [0, 2, 0],
+      [0, 2, 2],
+    ];
+  }else if(type==="J"){
+    return[
+      [0, 3, 0],
+      [0, 3, 0],
+      [3, 3, 0],
+    ];
+  }else if(type==="O"){
+    return[
+      [4, 4],
+      [4, 4],
+    ];
+  }else if(type==="Z"){
+    return[
+      [5, 5, 0],
+      [0, 5, 5],
+      [0, 0, 0],
+    ];
+  }else if(type==="S"){
+    return[
+      [0, 6, 6],
+      [6, 6, 0],
+      [0, 0, 0],
+    ];
+  }else if(type==="T"){
+    return[
+      [0, 7, 0],
+      [7, 7, 7],
+      [0, 0, 0],
+    ];
+  }
 }
